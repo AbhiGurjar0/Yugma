da9c347789db9a615440c6942ec96609a8c80b77
AbhiGurjar <abhishekbainsla190@gmail.com>
2025-07-16 18:18:29 +0530

    merged ✅

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.get('/dash', (req, res) => {
+    res.render('dashboard');
+});
+app.get('/App', (req, res) => {
+    res.render('App');
+});
 module.exports = app;
